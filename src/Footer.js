4f44e8b07c39f82784fb51d1a87d1ff0b4787c69
MehriS <meshokouhi@gmail.com>
2019-10-02 13:11:33 -0700

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; 
+
+const Footer = () => { 
+    return(
+        <div className='footer'>
+
+        <p> &copy; 2019 Mehri Shokouhi - Credit: <a href="https://newsapi.org/"> News API </a></p>
+        
+        </div>
+    );
+}
+export default Footer;
+
+
+
