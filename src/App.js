fbe116b191f4253d4208c894fcf66bbd83542c00
MehriS <meshokouhi@gmail.com>
2019-10-02 13:11:28 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } 
+                    from "react-router-dom";
+import Home         from "./Home";
+import About        from "./About.js";
+import NotFound     from './NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return  (
+    <Router>
+      <div className = 'wrapper'>
+        <ul className = 'navigation'>
+          <li><Link to="/news/about">About</Link></li>
+          <li><Link to="/news">Main</Link></li>
+        </ul>
+
+        {/* Our router goes here */}
+        <Switch> 
+        <Route exact path="/news/" component={Home} /> 
+        <Route exact path={'/news/about'} component={About} />
+        {/* Shows an error page. */}
+        <Route path="*" component={NotFound} />
+        </Switch>
+
+      </div>
+    </Router>);
+  }
 }
-
 export default App;
